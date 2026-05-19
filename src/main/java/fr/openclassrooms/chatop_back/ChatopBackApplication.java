5cc86d1c17e81287790a68edc444c0e78f20e385
romainsessa <romainsessa@gmail.com>
2026-05-19 21:53:19 +0200

    Start project

---
+++ src/main/java/fr/openclassrooms/chatop_back/ChatopBackApplication.java
@@ -0,0 +1,13 @@
+package fr.openclassrooms.chatop_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatopBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/openclassrooms/chatop_back/ChatopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.openclassrooms.chatop_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
